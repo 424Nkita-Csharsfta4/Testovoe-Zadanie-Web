d36f057d03c915eed5cf063ce65db85dc9f7c63c
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2022-05-03 14:05:31 +0300

    Delete Новая папка directory

--- Новая папка/phpFunctions/errors.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function AddError($connect, $error, $errorDesc = null){
-        $date = date('Y-m-d H:i:s');
-        $query =  $connect->query("INSERT INTO `errors` (`id`, `status`, `date_time`, `error`, `error_desc`) VALUES (NULL, 'error', '".$date."', '".$error."', '".$errorDesc."');");
-        echo $errorDesc."<br>";
-        die("<span style=font-size:16px;><b>Error:</b> ".$error."</span>");
-    }
-?>
